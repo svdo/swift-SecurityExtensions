e676c9c74c7385006cf3ec39499a6fb4899bd867
Stefan van den Oord <soord@mac.com>
2016-08-05 12:02:57 +0200

    Empty project skeleton

---
+++ SecurityExtensions/SecurityExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SecurityExtensions.h
+//  SecurityExtensions
+//
+//  Created by Stefan on 05/08/16.
+//  Copyright © 2016 Stefan van den Oord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SecurityExtensions.
+FOUNDATION_EXPORT double SecurityExtensionsVersionNumber;
+
+//! Project version string for SecurityExtensions.
+FOUNDATION_EXPORT const unsigned char SecurityExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecurityExtensions/PublicHeader.h>
+
+
